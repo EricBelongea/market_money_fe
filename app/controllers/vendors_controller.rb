d5192e1cf43fd9d643a04bed6bc57edceee96d67
Eric Belongea <eric.belongea@gmail.com>
2023-12-14 16:38:39 -0700

    Market facade testing

--- app/controllers/vendors_controller.rb
+++
@@ -1,5 +0,0 @@
-class VendorsController < ApplicationController
-  def show
-    @vendor = VendorFacade.vendor_show(params[:id])
-  end
-end
--- app/facades/vendor_facade.rb
+++
@@ -1,6 +0,0 @@
-class VendorFacade
-  def self.vendor_show(id)
-    json_response = VendorService.vendor_show(id)
-    Vendor.new(json_response[:data])
-  end
-end
