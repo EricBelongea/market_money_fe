3908194fd02e64b3502ceb56b76fe68a96dd28c3
Eric Belongea <eric.belongea@gmail.com>
2023-12-14 15:46:48 -0700

    Index feature spec markets

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,15 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Markets Index" do
-  before(:each) do
-
-  end
-
   it "Index page should show all markets", :vcr do
     visit markets_path
 
+    @markets = MarketFacade.all_markets
+    @market = @markets.first
+
     expect(page).to have_content("All Markets")
+    expect(page).to have_content(@market.name)
+    expect(page).to have_content(@market.city)
+    expect(page).to have_content(@market.state)
 
     expect(page).to have_link("More Info")
+
+    first_more_info_link = page.all('a', text: 'More Info').first
+    first_more_info_link.click
+
+    expect(current_path).to eq(market_path(@market.id))
   end
 end
