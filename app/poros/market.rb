f9d1ba0c178ff0a15b0b4ab6d5edfa4e3696e548
Eric Belongea <eric.belongea@gmail.com>
2023-12-14 15:34:55 -0700

    Updates routes and config

--- app/poros/market.rb
+++ app/poros/market.rb
@@ -1,16 +1,11 @@
 class Market
-  attr_reader :id, :name, :city, :state, :street, :county, :zip, :lat, :lon, :vendor_count, :type
-
+  attr_reader :id, :name, :city, :state, :street, :zip
   def initialize(data)
     @id = data[:id]
     @name = data[:attributes][:name]
     @city = data[:attributes][:city]
     @state = data[:attributes][:state]
     @street = data[:attributes][:street]
-    @county = data[:attributes][:county]
     @zip = data[:attributes][:zip]
-    @lat = data[:attributes][:lat]
-    @lon = data[:attributes][:lon]
-    @vendor_count = data[:attributes][:vendor_count]
   end
 end
